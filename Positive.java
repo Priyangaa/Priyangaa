a0776f668224102b29c8c836a90a0dcae58c1bac
Priyangaa <priyangaa.mcse@ksrce.ac.in>
2016-07-22 14:59:28 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,15 @@
+# Priyangaa
+class Positive
+{
+public static void main(String[] args)
+{
+int n=9;
+if(n>0)
+{
+System.out.println("num id +ve");
+}
+else
+{
+System.out.println("num is -Ve");
+}
+}
