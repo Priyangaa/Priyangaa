2036498b40abae401111a459c2e8b66364e94c1e
Priyangaa <priyangaa.mcse@ksrce.ac.in>
2016-07-22 15:08:12 +0530

    Create Vowels.java

---
+++ Vowels.java
@@ -0,0 +1,21 @@
+class Vowels
+{
+public static void main()
+{
+char c;
+switch(c)
+{
+case 'a':System.out.println("vowels");
+break;
+case 'e':System.out.println("vowels");
+break;
+case 'i':System.out.println("vowels");
+break;
+case 'o':System.out.println("vowels");
+break;
+case 'u':System.out.println("vowels");
+break;
+default:System.out.println("consonants");
+}
+}
+}
