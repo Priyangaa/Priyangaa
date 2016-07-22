57fd71a6fbfef4604fd1d76aa879008dffea88fb
Priyangaa <priyangaa.mcse@ksrce.ac.in>
2016-07-22 15:13:58 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,63 @@
+class Alphabet
+{
+public static void main(String[] args)
+{
+char c;
+switch(c)
+{
+case 'a':System.out.println("alphabet");
+break;
+case 'b':System.out.println("alphabet");
+break;
+case 'c':System.out.println("alphabet");
+break;
+case 'd':System.out.println("alphabet");
+break;
+case 'e':System.out.println("alphabet");
+break;
+case 'f':System.out.println("alphabet");
+break;
+case 'g':System.out.println("alphabet");
+break;
+case 'h':System.out.println("alphabet");
+break;
+case 'i':System.out.println("alphabet");
+break;
+case 'j':System.out.println("alphabet");
+break;
+case 'k':System.out.println("alphabet");
+break;
+case 'l':System.out.println("alphabet");
+break;
+case 'm':System.out.println("alphabet");
+break;
+case 'n':System.out.println("alphabet");
+break;
+case 'o':System.out.println("alphabet");
+break;
+case 'p':System.out.println("alphabet");
+break;
+case 'q':System.out.println("alphabet");
+break;
+case 'r':System.out.println("alphabet");
+break;
+case 's':System.out.println("alphabet");
+break;
+case 't':System.out.println("alphabet");
+break;
+case 'u':System.out.println("alphabet");
+break;
+case 'v':System.out.println("alphabet");
+break;
+case 'w':System.out.println("alphabet");
+break;
+case 'x':System.out.println("alphabet");
+break;
+case 'y':System.out.println("alphabet");
+break;
+case 'z':System.out.println("alphabet");
+break;
+default:System.out.println(" not an alphabet");
+}
+}
+}
