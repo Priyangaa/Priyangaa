51d63f0e1214e4735dfa4aa6fd2e78ad0fe721a2
Priyangaa <priyangaa.mcse@ksrce.ac.in>
2016-07-22 15:17:03 +0530

    Create Large.java

---
+++ Large.java
@@ -0,0 +1,21 @@
+class Large
+{
+public static void main()
+{
+int a=7;
+int b=2;
+int c=9;
+if(a>b&&a>c)
+{
+System.out.println("a is large");
+}
+else if(b>a&&b>c)
+{
+System.out.println("b is big");
+}
+else
+{
+System.out.println("c is big");
+}
+}
+}
